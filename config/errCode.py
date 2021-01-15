a9ea09ed0b486254e1ae7027fc6fb44f9a5ac55a
pjy1368 <basejin0810@naver.com>
2021-01-15 14:41:40 +0900

    fix: 딕셔너리 문법 오류 수정.

--- config/errCode.py
+++ config/errCode.py
@@ -2,6 +2,6 @@ def errors(err_code):
     err_dic = {0: ('OP_ERR_NONE', '정상처리'),
                -100: ('OP_ERR_LOGIN', '사용자정보교환실패'),
                -101: ('OP_ERR_CONNECT', '서버접속실패'),
-               -102('OP_ERR_VERSION', '버전처리실패')}
+               -102: ('OP_ERR_VERSION', '버전처리실패')}
     result = err_dic[err_code]
     return result
