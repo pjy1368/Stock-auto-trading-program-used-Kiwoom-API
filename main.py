59dd3a0fe2c8aeb06d0d17331e003d5efeefd3fd
pjy1368 <basejin0810@naver.com>
2021-01-17 13:14:58 +0900

    style: 들여쓰기 수정.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 from kiwoom.kiwoom import *
 from PyQt5.QtWidgets import *
 
+
 class Main():
     def __init__(self):
-        self.app = QApplication(sys.argv) # QApplication 객체 생성.
+        self.app = QApplication(sys.argv)  # QApplication 객체 생성.
         self.kiwoom = Kiwoom()
-        self.app.exec_() # 이벤트 루프 실행.
+        self.app.exec_()  # 이벤트 루프 실행.
+
 
 if __name__ == "__main__":
     Main()
